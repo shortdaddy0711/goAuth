87cec54588906515d829246dd9d405599db4542c
shortdaddy0711 <sodoatm@gmail.com>
2021-03-10 17:23:07 -0800

    final commit

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/shortdaddy0711/goAuth
 go 1.16
 
 require (
-	cloud.google.com/go v0.78.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/pat v1.0.1
 	github.com/urfave/negroni v1.0.0
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
 	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
+	google.golang.org/appengine v1.6.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,84 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/pat"
 	"github.com/urfave/negroni"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 var googleOAuthConfig = oauth2.Config{
-
+	RedirectURL:  "http://localhost:8000/auth/google/callback",
+	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_SECRET_KEY"),
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
 }
 
 func googleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	state := generateStateOAuthCookie(w)
+	url := googleOAuthConfig.AuthCodeURL(state)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
 
+func generateStateOAuthCookie(w http.ResponseWriter) string {
+	expiration := time.Now().Add(1 * 24 * time.Hour)
+	// declare empty array of byte size 16
+	b := make([]byte, 16)
+	// fill the array with randome byte
+	rand.Read(b)
+	// encode url
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := &http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, cookie)
+	return state
+}
+
+func googleOAuthCallBack(w http.ResponseWriter, r *http.Request) {
+	oauthstate, _ := r.Cookie("oauthstate")
+	if r.FormValue("state") != oauthstate.Value {
+		log.Printf("invalid google oauth state cookie: %s\n state:%s\n", oauthstate.Value, r.FormValue("state"))
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	}
+
+	data, err := getGoogleUserInfo(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+	}
+
+	fmt.Fprint(w, string(data))
+
+}
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+func getGoogleUserInfo(code string) ([]byte, error) {
+	token, err := googleOAuthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("Fail to Exchange %s\n", err.Error())
+	}
+	resp, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get user info %s\n", err.Error())
+	}
+	return ioutil.ReadAll(resp.Body)
 }
 
 func main() {
 	mux := pat.New()
 	mux.HandleFunc("/auth/google/login", googleLoginHandler)
+	mux.HandleFunc("/auth/google/callback", googleOAuthCallBack)
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
